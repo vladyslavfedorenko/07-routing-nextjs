dd5b7eead9f78747290c29f8a4fac82753dce476
vladyslav_fedorenko <godforgiveme1993@gmail.com>
2025-10-24 12:52:56 +0300

    changed

--- app/notes/[id]/NoteDetails.client.tsx
+++ app/notes/[id]/NoteDetails.client.tsx
@@ -2,36 +2,41 @@
 
 import { useQuery } from "@tanstack/react-query";
 import { fetchNoteById } from "@/lib/api";
-import styles from "./NoteDetails.module.css";
+import { useRouter } from "next/navigation";
+import css from "./NoteDetails.module.css";
+import type { Note } from "@/types/note";
 
-interface Props {
-  id: string;
-}
+export default function NoteDetails({ id }: { id: string }) {
+  const router = useRouter();
 
-export default function NoteDetailsClient({ id }: Props) {
   const {
     data: note,
     isLoading,
     isError,
-  } = useQuery({
+  } = useQuery<Note>({
     queryKey: ["note", id],
     queryFn: () => fetchNoteById(id),
     refetchOnMount: false,
   });
 
-  if (isLoading) return <p>Loading, please wait...</p>;
-  if (isError || !note) return <p>Something went wrong.</p>;
+  if (isLoading) return <p>Loading note...</p>;
+  if (isError || !note) return <p>Failed to load note.</p>;
 
   return (
-    <div className={styles.container}>
-      <div className={styles.item}>
-        <div className={styles.header}>
-          <h2>{note.title}</h2>
-        </div>
-        <p className={styles.content}>{note.content}</p>
-        <p className={styles.date}>
-          Created: {new Date(note.createdAt).toLocaleDateString()}
+    <div className={css.wrapper}>
+      <div className={css.card}>
+        <h2 className={css.title}>{note.title}</h2>
+        <p className={css.content}>{note.content}</p>
+        <p className={css.tag}>Tag: {note.tag}</p>
+        <p className={css.date}>
+          Created:{" "}
+          {note.createdAt
+            ? new Date(note.createdAt).toLocaleDateString()
+            : "Unknown date"}
         </p>
+        <button className={css.button} onClick={() => router.back()}>
+          Close
+        </button>
       </div>
     </div>
   );
